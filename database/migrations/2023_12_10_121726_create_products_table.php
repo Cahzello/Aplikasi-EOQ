bd7a3586aa61eb41566e61e0f1cb71a84908a3d8
Cahzello <rizkyrasya.m@gmail.com>
2023-12-10 19:33:51 +0700

    membuat migrations

---
+++ database/migrations/2023_12_10_121726_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('bahan_baku');
+            $table->string('total_penggunaan_tahunan');
+            $table->string('max_penggunaan_tahunan');
+            $table->string('average_penggunaan_tahunan');
+            $table->string('biaya_pemesanan');
+            $table->string('biaya_penyimpanan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- database/migrations/2023_12_10_122633_create_calculates_table.php
+++ database/migrations/2023_12_10_122633_create_calculates_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('calculates', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
+            $table->foreignId('product_id')->constrained();
+            $table->integer('eoq');
+            $table->decimal('rop');
+            $table->decimal('safety_stock');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('calculates');
     }
 };
