b6ed02016cd2d00ef2dd8116940ce9ba0b93ee8b
Cahzello <rizkyrasya.m@gmail.com>
2023-12-26 08:28:10 +0700

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,15 +21,15 @@ class DatabaseSeeder extends Seeder
         User::create([
             'username' => 'rizky',
             'email' => 'cahzello@gmail.com',
-            'password'=> bcrypt('password'),
+            'password' => bcrypt('password'),
             'role' => 'admin'
-        
+
         ]);
 
         User::create([
             'username' => 'yukina minato',
             'email' => 'yukina@gmail.com',
-            'password'=> bcrypt('password'),
+            'password' => bcrypt('password'),
             'role' => 'user'
         ]);
 
@@ -38,14 +38,21 @@ class DatabaseSeeder extends Seeder
             'bahan_baku' => 'Coklat'
         ]);
 
-        $bulan = ['januari', 'februari', 'maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'september', 'oktober', 'november', 'desember'];
+        $data = [
+            'bulan' => ['januari', 'februari', 'maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'september', 'oktober', 'november', 'desember'],
+            'beli' => [1800, 1900, 1800, 1950, 1950, 1850, 1900, 1850, 1800, 1700, 1750, 1800],
+            'penggunaan' => [1950, 1750, 1650, 1790, 1950, 1900, 1925, 1725, 1830, 1650, 1530, 1900]
+        ];
 
-        foreach ($bulan as $value) {
+        foreach ($data['bulan'] as $i => $bulan) {
+            $beli = $data['beli'][$i];
+            $penggunaan = $data['penggunaan'][$i];
+        
             Item_detail::create([
                 'item_id' => 1,
-                'bulan' => $value,
-                'jumlah_pembelian' => rand(1, 99),
-                'jumlah_penggunaan'=> rand(1, 99),
+                'bulan' => $bulan,
+                'jumlah_pembelian' => $beli,
+                'jumlah_penggunaan' => $penggunaan,
                 'biaya_pemesanan' => rand(1, 99),
                 'biaya_penyimpanan' => rand(1, 99),
                 'leadtime' => rand(1, 9),
